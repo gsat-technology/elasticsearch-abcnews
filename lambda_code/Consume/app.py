1199b69c0a5648000f59d35ad85f801334f4d751
gsat-technology <gsat.technology@gmail.com>
2017-02-04 08:55:57 +1100

    updates lambdas and cf

--- lambda_code/Consume/app.py
+++ lambda_code/Consume/app.py
@@ -10,6 +10,8 @@ table = dynamodb.Table(dynamodb_table)
 
 def handler(event, context):
 
+    print(event)
+
     response = {
                 "headers": {},
                 "statusCode": None,
@@ -18,21 +20,17 @@ def handler(event, context):
 
     if event['httpMethod'] == 'POST':
         payload = json.loads(event['body'])
-        print(payload)
-
-        #payload sample:  {"time": {"updated":"Feb 2, 2017 00:25:00 UTC","updatedISO":"2017-02-02T00:25:00+00:00","updateduk":"Feb 2, 2017 at 00:25 GMT"},"disclaimer":"This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org","bpi":{"USD":{"code":"USD","rate":"989.2125","description":"United States Dollar","rate_float":989.2125},"AUD":{"code":"AUD","rate":"1,304.5082","description":"Australian Dollar","rate_float":1304.5082}}
-
-        #format data to put in db
-        payload['updatedISO'] = payload['time']['updatedISO']
-        payload.pop('disclaimer', None) #disclaimer not required
-        payload.pop('time', None) #time not required (we'll just use updatedISO)
-        payload['bpi']['AUD'].pop('rate_float', None)
-        payload['bpi']['USD'].pop('rate_float', None)
 
         result = table.put_item(Item=payload)
 
         if result['ResponseMetadata']['HTTPStatusCode'] == 200:
             response['statusCode'] = 204
+
+            #identifier for subscriptions filter
+            payload['logstream_identifier'] = 'abc_news_data'
+
+            #log to cw logs (subscription will filter this and add to ES)
+            print(json.dumps(payload))
         else:
             response['statusCode'] = 200
             response['body'] = json.dumps({'result': 'ERROR'})
--- lambda_code/Generate/app.py
+++ lambda_code/Generate/app.py
@@ -1,22 +1,120 @@
 import os
+import json
+
+import boto3
+import arrow
+import xmltodict
 import requests
 
-bc_url = os.environ['bitcoin_url']
-apig = 'https://{}.execute-api.{}.amazonaws.com/{}/consume'.format(os.environ['apig_id'], os.environ['region'], os.environ['apig_stage'])
+#environment variables
+abc_rss_feed = os.environ['abc_rss_feed']
+apig_stage = os.environ['apig_stage']
+region = os.environ['region']
+apig_id = os.environ['apig_id']
+s3_latest_record_bucket = os.environ['s3_latest_record_bucket']
+
+apig_endpoint = 'https://{}.execute-api.{}.amazonaws.com/{}/consume'.format(apig_id, region, apig_stage)
+record_object = 'record.txt'
+
+FORMAT = 'ddd, DD MMM YYYY HH:mm:ss ZZ'
+
+
+s3 = boto3.resource('s3')
+s3_client = boto3.client('s3')
+
+def generate_id(timestr, f):
+    return arrow.get(timestr, f).timestamp
+
+
+def creator_or_blank(item):
+    if 'dc:creator' in item:
+        return item['dc:creator']
+    else:
+        return "-"
+
+
+def categories_or_blank(item):
+    if 'category' in item:
+        return item['category']
+    else:
+        return "-"
+
+
+def send_to_apig(endpoint, item):
+    print(item)
+    r = requests.post(endpoint, data=json.dumps(item))
+    print('result status: {}'.format(r.status_code))
+    print('result: {}'.format(r.text))
+
+
+
+def process_item(item, previous_record):
+
+    _id = generate_id(item['pubDate'], FORMAT)
+
+    if _id > previous_record or previous_record == None:
+
+        print('processing item with id: {}'.format(_id))
+
+        return {
+            'id': _id,
+            'title': item['title'],
+            'link': item['link'],
+            'description': item['description'].split('<p>')[1].split('</p>')[0],
+            'creator': creator_or_blank(item),
+            'pub_date': item['pubDate'],
+            'categories': categories_or_blank(item)
+        }
+    else:
+        return None
+
+
+#get the latest record (which is a timestamp) from when this function was last run
+def get_previous_record(bucket, key):
+
+    try:
+        obj = s3.Object(s3_latest_record_bucket, key)
+        record_str = obj.get()['Body'].read().decode('utf-8')
+        record = int(record_str)
+        return record
+    except:
+        return None
+
+
+#write the current record to s3
+def write_current_record(timestamp, bucket, key):
+
+    print('writing new record to s3: {}'.format(timestamp))
+    open('/tmp/' + key, 'w').write(str(timestamp))
+    result = s3_client.upload_file('/tmp/' + key, bucket, key)
+    print(result)
 
 
 def handler(event, context):
 
-    bc_data = requests.get(bc_url).text
+    previous_record = get_previous_record(s3_latest_record_bucket, record_object)
 
-    print(bc_data)
-
-    r = requests.post(apig, data=bc_data)
-
-    if str(r.status_code)[0] == '2':
-        print('apig result OK')
+    if previous_record:
+        print('previous record timestamp: {}'.format(previous_record))
     else:
-        print('ERROR. status code {}'.format(r.status_code))
-        print('reason: {}'.format(r.text))
+        print('no previous record')
 
-    return 'done'
+    r = requests.get(abc_rss_feed)
+    doc = xmltodict.parse(r.text)
+
+    item_objects = [ process_item(doc_item, previous_record) for doc_item in doc['rss']['channel']['item'] ]
+    item_objects = filter(None, item_objects) #remove 'None'
+
+    if item_objects:
+        print('{} objects'.format(len(item_objects))) #print item count
+
+        #write latest timestamp to s3
+        timestamps = [ i['id'] for i in item_objects ]
+        max_timestamp = max(timestamps)
+        write_current_record(max_timestamp, s3_latest_record_bucket, record_object )
+    else:
+        print('0 new items')
+
+    #sent to APIG
+    for item in item_objects:
+        send_to_apig(apig_endpoint, item)
